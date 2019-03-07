0f643551d44dcbd15a03068a5b9e5c36509a71f9
Jordan Babbitt <jordanbabbitt@Jordans-MacBook-Pro.local>
2019-03-06 19:12:06 -0800

    Javascript Error but working everything else

---
+++ assets/actions/game.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+    var Random = Math.floor(Math.random() * 101 + 19)
+    // Selects a random number to be shown at the start of the game
+    // Number should be should be between 19 - 120
+    //
+    $("#randomNumber").text(Random);
+    // Appending random number to the randomNumber id in the html doc
+    //
+    var num1 = Math.floor(Math.random() * 11 + 1)
+    var num2 = Math.floor(Math.random() * 11 + 1)
+    var num3 = Math.floor(Math.random() * 11 + 1)
+    var num4 = Math.floor(Math.random() * 11 + 1)
+    // Setting up random numbers for each jewel
+    // Random number has to be between 1 - 12
+    // 
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    //  Decaring variables for tallies
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    //resets the game
+    function reset() {
+        Random = Math.floor(Math.random() * 101 + 19);
+        console.log(Random)
+        $("#randomNumber").text(Random);
+        num1 = Math.floor(Math.random() * 11 + 1);
+        num2 = Math.floor(Math.random() * 11 + 1);
+        num3 = Math.floor(Math.random() * 11 + 1);
+        num4 = Math.floor(Math.random() * 11 + 1);
+        userTotal = 0;
+        $("#totalScore").text(userTotal);
+    }
+    //adds the wins to the userTotal
+    function yay() {
+        alert("You won!");
+        wins++;
+        $("#numberWins").text(wins);
+        reset();
+    }
+    //addes the losses to the userTotal
+    function loser() {
+        alert("You lose!");
+        losses++;
+        $("#losses").text(losses);
+        reset()
+    }
+    //sets up click for jewels
+    $("#Crystal1").on('click', function () {
+        userTotal = userTotal + num1;
+        console.log("New userTotal= " + userTotal);
+        $("#totalScore").text(userTotal);
+        //sets win/lose conditions
+        if (userTotal == Random) {
+            yay();
+        }
+        else if (userTotal > Random) {
+            loser();
+        }
+    })
+    $("#Crystal2").on('click', function () {
+        userTotal = userTotal + num2;
+        console.log("New userTotal= " + userTotal);
+        $("#totalScore").text(userTotal);
+        if (userTotal == Random) {
+            yay();
+        }
+        else if (userTotal > Random) {
+            loser();
+        }
+    })
+    $('#Crystal3').on('click', function () {
+        userTotal = userTotal + num3;
+        console.log("New userTotal= " + userTotal);
+        $("#totalScore").text(userTotal);
+        //sets win/lose conditions
+        if (userTotal == Random) {
+            yay();
+        }
+        else if (userTotal > Random) {
+            loser();
+        }
+    })
+    $('#Crystal4').on('click', function () {
+        userTotal = userTotal + num4;
+        console.log("New userTotal= " + userTotal);
+        $("#totalScore").text(userTotal);
+
+        if (userTotal == Random) {
+            yay();
+        }
+        else if (userTotal > Random) {
+            loser();
+        }
+    });
+});
+
+//beginGame
